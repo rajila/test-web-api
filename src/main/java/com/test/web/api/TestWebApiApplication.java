68da128c37500de646e73b036b7ce863229e6ab4
rajila <rajila@espol.edu.ec>
2018-07-21 19:50:44 +0200

    Proyecto de prueba de Spring

---
+++ src/main/java/com/test/web/api/TestWebApiApplication.java
@@ -0,0 +1,12 @@
+package com.test.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebApiApplication.class, args);
+	}
+}
